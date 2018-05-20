79e491045bf40ffc31cc10c14271375912d31573
vita <dragic.hsieh@gmail.com>
2018-05-20 23:14:51 +0800

    Done setup post resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # 設定前台路由
+  # 設定前台首頁
   root "posts#index"
+
+  # 設定前台路由
+  resources :posts, only: [:index, :show]
 
   # 設定後台路由，運用 Namespace 這個方法
   namespace :admin do
