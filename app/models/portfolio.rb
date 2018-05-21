29f81937e157a51913f378db22491609efb96120
vita <dragic.hsieh@gmail.com>
2018-05-21 19:38:27 +0800

    Done add new action and view page then modify file name

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,9 @@
 class Portfolio < ApplicationRecord
 
+	# 這是carrierwave提供的方法
 	mount_uploader :image, PhotoUploader
+
+	# 驗證 title、content 及 image 欄位必填
+	validates :title, :content, :image, presence: true
 
 end
