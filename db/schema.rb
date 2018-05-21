79fcf86d485a9d648fef47f1d7f4b1b0aaceaf91
vita <dragic.hsieh@gmail.com>
2018-05-21 18:05:08 +0800

    Done add Portfolio model then db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517163009) do
+ActiveRecord::Schema.define(version: 20180521100136) do
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
