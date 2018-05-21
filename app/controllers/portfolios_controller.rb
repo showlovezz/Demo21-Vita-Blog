785d73216ddb72ae0fc6671ae12543de47168d6b
vita <dragic.hsieh@gmail.com>
2018-05-22 00:34:12 +0800

    Done show action and show view for public users

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,11 @@
 class PortfoliosController < ApplicationController
 
-def index
-	@portfolios = Portfolio.all
-end
+	def index
+		@portfolios = Portfolio.all
+	end
+
+	def show
+		@portfolio = Portfolio.find(params[:id])
+	end
 
 end
