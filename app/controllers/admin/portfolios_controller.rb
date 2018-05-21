e9d8b039f26a2a2e067340088f5e36a89d8274b2
vita <dragic.hsieh@gmail.com>
2018-05-21 20:38:54 +0800

    Done destroy action

--- app/controllers/admin/portfolios_controller.rb
+++ app/controllers/admin/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class Admin::PortfoliosController < ApplicationController
 
-	before_action :find_portfolio, only: [:show, :edit]
+	before_action :find_portfolio, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@portfolios = Portfolio.all
@@ -26,6 +26,22 @@ class Admin::PortfoliosController < ApplicationController
 
 	def edit
 	end
+
+	def update
+		if @portfolio.update(portfolio_params)
+			flash[:notice] = "作品修改成功"
+			redirect_to admin_portfolios_path
+		else
+			flash[:alert] = "少輸入什麼，作品修改不成功"
+			render 'edit'
+		end
+	end
+	def destroy
+		@portfolio.destroy
+		flash[:alert] = "作品被刪除了"
+		redirect_to admin_portfolios_path
+	end
+
 
 	private
 
