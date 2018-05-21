05547b0039eca54f9fc77eab6c394b472a55eb46
vita <dragic.hsieh@gmail.com>
2018-05-21 19:48:31 +0800

    Done create action for admin

--- app/controllers/admin/portfolios_controller.rb
+++ app/controllers/admin/portfolios_controller.rb
@@ -7,5 +7,22 @@ class Admin::PortfoliosController < ApplicationController
 	def new
 		@portfolio = Portfolio.new
 	end
+
+	def create
+		@portfolio = Portfolio.new(portfolio_params)
+		if @portfolio.save
+			flash[:notice] = "作品建立成功"
+			redirect_to admin_portfolios_path
+		else
+			flash[:alert] = "少輸入什麼，作品建立不成功"
+			render 'new'
+		end
+	end
+
+	private
+
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :content, :image)
+	end
 	
 end
